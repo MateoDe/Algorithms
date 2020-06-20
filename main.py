77931a96c149c0deb0b5571326b8c91c23104b49
MateoDe <mateodellacasa00@gmail.com>
2020-06-20 19:14:40 -0300

    FIles

---
+++ main.py
@@ -0,0 +1,26 @@
+import random
+
+def binary_search(lista, beginning, end, objective):
+    if beginning > end:
+        return False
+    
+    middle = (beginning + end) // 2
+
+    if lista[middle] == objective:
+        return True
+    elif lista[middle] < objective:
+        return binary_search(lista, middle + 1, end, objective)
+    else:
+        return binary_search(lista, beginning, middle - 2, objective)
+
+
+if __name__ == '__main__':
+    size_of_the_list = int(input('What size does the list have: '))
+    objective = int(input('What number is your objective? '))
+
+    lista = sorted([random.randint(0, 100) for i in range(size_of_the_list)]) #The sorted list is created 
+
+    found = binary_search(lista, 0, len(lista), objective)
+
+    print(lista)
+    print(f'The element {objective} {"is" if found else "was not found"} in the list')
